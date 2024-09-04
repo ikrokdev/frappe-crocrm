ec6fcb038e1201e949979cfb41e89d8753a6e7c1
Roman <roman.s@ikrok.net>
2024-09-04 08:46:56 +0000

    Revert "feat(quick_list_multiple): added widget"
    This reverts commit f052cb9ee6548e3b4131e5b0fee215826c7520b8.

--- frappe/public/js/frappe/views/workspace/workspace.js
+++ frappe/public/js/frappe/views/workspace/workspace.js
@@ -1428,12 +1428,6 @@ frappe.views.Workspace = class Workspace {
 					page_data: this.page_data || [],
 				},
 			},
-			quick_list_multiple: {
-				class: this.blocks["quick_list_multiple"],
-				config: {
-					page_data: this.page_data || [],
-				},
-			},
 			number_card: {
 				class: this.blocks["number_card"],
 				config: {
